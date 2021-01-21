855a59fe25363f0e5a6f14558e4115cda1ab9f4d
Stas <sgranin@gmail.com>
2021-01-21 08:43:24 +0200

    Modified the privateRoute component

--- src/components/privateRoute.component/privateRoute.component.jsx
+++ src/components/privateRoute.component/privateRoute.component.jsx
@@ -1,13 +1,13 @@
 'use strict';
 
+import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 
-const PrivateRoute = ({component: Component, auth, ...rest}) => (
-	<Route {...rest} render={props => (isLoggedIn === true ? <Component {...props} /> : <Redirect to="/login" />)} />
-);
+const PrivateRoute = ({component: Component, isLoggedIn, ...rest}) =>
+	(<Route {...rest} render={props => (isLoggedIn === true ? <Component {...props} /> : <Redirect to="/login" />)} />);
 
 const mapStateToProps = state => ({isLoggedIn: state.auth.isLoggedIn});
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
