b0fbf39a154d7490df9f426c15692cdc2ec0fe5c
Stas <sgranin@gmail.com>
2021-01-24 08:46:24 +0200

    Implemented global filter on main page

--- src/components/pages/mainPage.component/mainPage.component.jsx
+++ src/components/pages/mainPage.component/mainPage.component.jsx
@@ -3,43 +3,53 @@
 import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {withRouter} from "react-router-dom";
 import moment from 'moment';
-
 import DynamicTable from '../../dynamicTable.component/dynamicTable.component.jsx';
 import {getUsers} from '../../../actions/users.actions';
-import {dataTypes} from '../../../services/filter.service';
+import FilterService, {dataTypes} from '../../../services/filter.service';
 import {texts} from '../../../services/language.service';
 
 import './mainPage.component.scss';
 
+const usersTableColumns = [
+	{key: 'id', dataType: dataTypes.NUMBER, label: texts.usersTableColumnLabels.ID},
+	{key: 'firstName', dataType: dataTypes.STRING, label: texts.usersTableColumnLabels.FIRST_NAME},
+	{key: 'lastName', dataType: dataTypes.STRING, label: texts.usersTableColumnLabels.LAST_NAME},
+	{key: 'date', dataType: dataTypes.DATE, label: texts.usersTableColumnLabels.DATE, dataFormat: data => moment(data).format('DD/MM/YYYY')},
+	{key: 'phone', dataType: dataTypes.STRING, label: texts.usersTableColumnLabels.PHONE},
+];
 
-const MainPage = ({users, getUsers}) => {
+const MainPage = ({users, isLoaded, getUsers, history}) => {
 
-	const usersTableColumns = [
-		{key: 'id', dataType: dataTypes.NUMBER, label: texts.usersTableColumnLabels.ID},
-		{key: 'firstName', dataType: dataTypes.STRING, label: texts.usersTableColumnLabels.FIRST_NAME},
-		{key: 'lastName', dataType: dataTypes.STRING, label: texts.usersTableColumnLabels.LAST_NAME},
-		{key: 'date', dataType: dataTypes.DATE, label: texts.usersTableColumnLabels.DATE, dataFormat: data => moment(data).format('DD/MM/YYYY')},
-		{key: 'phone', dataType: dataTypes.STRING, label: texts.usersTableColumnLabels.PHONE},
-	];
+	const [filteredUsers, setFilteredUsers] = useState([]);
+	const [filter, setFilter] = useState(FilterService.createEmptyFilter({dataType: dataTypes.STRING}));
+	const handleTableRowClick = userId => history.push(`edit/${userId}`);
+	const handleFilterChange = (event) => setFilter({...setFilter, value: event.target.value});
+	const filterUsers = () => setFilteredUsers([...FilterService.filterArray(users, filter)]);
 
-	useEffect(()=> {getUsers()}, []);
+	useEffect(() => {getUsers()}, []);
+	useEffect(() => filterUsers(), [users, filter]);
 
 	return (
 		<div className="mainPage">
-			<div className="panel globalFilterBox">
+			{isLoaded && <>
+				<div className="pageTitle">{texts.pageHeaders.MAIN_PAGE}</div>
 
+			<div className="panel globalFilterBox">
+				<input name="globalFilter" placeholder={texts.filters.FILTER} value={filter.value} onChange={handleFilterChange} />
 			</div>
 
 			<div className="panel usersTable">
-				<DynamicTable tableColumns={usersTableColumns} tableData={users} />
+				<DynamicTable tableColumns={usersTableColumns} tableData={filteredUsers} onRowClick={handleTableRowClick} />
 			</div>
+			</> || <div className="pageTitle">{texts.pageHeaders.LOADING}</div>}
 
 		</div>
 	);
 };
 
-const mapStateToProps = state => ({users: state.users.users});
+const mapStateToProps = state => ({users: state.users.users, isLoaded: state.users.isLoaded});
 const mapDispatchToProps = dispatch => bindActionCreators({getUsers}, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MainPage));
